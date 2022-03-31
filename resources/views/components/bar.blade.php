1c0dd6ddd0a0bd94c7d6d1c4a70632d4e436485b
Victor Lascares <vmlascares@gmail.com>
2022-03-30 19:06:20 -0600

    Agregando estructura principal de carpetas e imagenes

---
+++ resources/views/components/bar.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-success bg-success">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('img/Logo_gob_mx.svg') }}" alt="">
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Iniciar Sesion</a>
+            </li>
+        </ul>
+    </div>
+</nav>
