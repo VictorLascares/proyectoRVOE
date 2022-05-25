b71d087410295d1421924fde8dd4ca3f2b999205
Victor Lascares <vmlascares@gmail.com>
2022-05-25 16:40:01 -0500

    Actualizando estilos de la barra superior

--- resources/views/components/bar.blade.php
+++ resources/views/components/bar.blade.php
@@ -1,12 +1,19 @@
-<div class="bar container-fluid d-flex justify-content-between align-items-center px-2 py-3">
-  <a href="{{ url('/') }}">
-    <img src="{{ asset('img/Logo_gob_mx.svg') }}" alt="">
-  </a>
-  @guest
-    <button type="button" data-bs-target="#loginModal" class="text-light" data-bs-toggle="modal" style="background-color:transparent">Iniciar Sesion</button>
-  @endguest
-  @auth
-    <a href="{{ url('logout') }}" class="text-light text-decoration-none" style="background-color:transparent">Cerrar
-      Sesión</a>
-  @endauth
+<div class="bar navbar-dark container-fluid px-3 py-3">
+  <div class="navbar-nav justify-content-between flex-row align-items-center">
+    <a href="{{ url('/') }}">
+      <img src="{{ asset('img/Logo_gob_mx.svg') }}" alt="">
+    </a>
+    @guest
+      <div class="nav-item">
+        <button type="button" data-bs-target="#loginModal" class="nav-link border-0" data-bs-toggle="modal"
+          style="background-color:transparent">Iniciar Sesion</button>
+      </div>
+    @endguest
+    @auth
+      <div class="nav-item">
+        <a href="{{ url('logout') }}" class="nav-link text-decoration-none" style="background-color:transparent">Cerrar
+          Sesión</a>
+      </div>
+    @endauth
+  </div>
 </div>
