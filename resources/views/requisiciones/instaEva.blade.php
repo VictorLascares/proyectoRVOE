cfd96b57ee18ad8e8b4756a31bf9001041fb0aee
Victor Lascares <vmlascares@gmail.com>
2022-05-29 18:23:37 -0500

    Actualizando estilos de formularios en el show de requisition

--- resources/views/requisiciones/instaEva.blade.php
+++ resources/views/requisiciones/instaEva.blade.php
@@ -4,62 +4,66 @@
   <x-navbar />
 @endsection
 @section('main-content')
-  <div class="container-lg pb-4 mb-4">
+  <div class="container-fluid pb-4 mb-4 px-4">
     <h1 class="text-center mt-3">Evaluación de Instalaciones</h1>
-    <form action="{{ url('/update/elements') }}" method="POST" class="p-4" enctype="multipart/form-data">
+    <form action="{{ url('/update/elements') }}" method="POST" enctype="multipart/form-data">
       @csrf
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Elemento</th>
-          <th scope="col">Existencia</th>
-          <th scope="col">Observacion</th>
-        </tr>
-      </thead>
-        <input type="hidden" name="requisition_id" value="{{ $requisition->id }}">
-        <tbody>
-          @foreach (range(1,52) as $i)
-            @foreach ($elements as $element)
-              @if ($element->elemento == $i)
-                <tr>
-                  <th scope="row">
-                    {{$i}}
-                  </th>
-                  <td class="w-50">
-                    <p>{{ $elementName[$i-1 ] }}</p>
-                  </td>
-                  <td>
-                    <div class="btn-group" role="group">
-                      <input type="radio" class="btn-check" name="elemento{{$i}}"
-                        value="{{ $element->existente }}" id="btnYes-{{ $element->id }}" autocomplete="off"
-                        @if ($element->existente) checked @endif>
-                      <label class="btn btn-outline-success text-uppercase" for="btnYes-{{ $element->id }}">Si</label>
-      
-                      <input type="radio" class="btn-check btn-No" name="elemento{{ $i }}"
-                        id="btnNo-{{ $element->id }}" value="{{ $element->existente }}" autocomplete="off"
-                        @if (!$element->existente) checked @endif>
-                      <label class="btn btn-outline-danger text-uppercase" for="btnNo-{{ $element->id }}">No</label>
-                    </div>
-                  </td>
-                  <td class="p-0">
-                    <div class="form-floating">
-                      <textarea rows="3" name="elemento{{$i}}o" class="form-control rounded-0 resize-none fs-6 border-0" placeholder="Observación" id="inputSighting-{{$element->id}}">{{$element->observacion}}</textarea>
-                      <label for="inputSighting-{{$element->id}}">Observación</label>
-                    </div>
-                  </td>
-                </tr>
-              @endif
+      <div class="table-wrapper-scroll-y my-custom-scrollbar">
+        <table id="elementsTable" class="table table-striped table-bordered table-sm">
+          <thead>
+            <tr>
+              <th class="th-sm" scope="col">#</th>
+              <th class="th-sm" scope="col">Elemento</th>
+              <th class="th-sm" scope="col">Existencia</th>
+              <th class="th-sm" scope="col">Observacion</th>
+            </tr>
+          </thead>
+          <input type="hidden" name="requisition_id" value="{{ $requisition->id }}">
+          <tbody>
+            @foreach (range(1, 52) as $i)
+              @foreach ($elements as $element)
+                @if ($element->elemento == $i)
+                  <tr>
+                    <th scope="row">
+                      {{ $i }}
+                    </th>
+                    <td class="w-50">
+                      <p class="h6">{{ $elementName[$i - 1] }}</p>
+                    </td>
+                    <td class="text-center" style="height: 100%">
+                      <div class="h-100 btn-group" role="group">
+                        <input type="radio" class="btn-check" name="elemento{{ $i }}"
+                          value="{{ $element->existente }}" id="btnYes-{{ $element->id }}" autocomplete="off"
+                          @if ($element->existente) checked @endif>
+                        <label class="btn btn-outline-success text-uppercase" for="btnYes-{{ $element->id }}">Si</label>
+
+                        <input type="radio" class="btn-check btn-No" name="elemento{{ $i }}"
+                          id="btnNo-{{ $element->id }}" value="{{ $element->existente }}" autocomplete="off"
+                          @if (!$element->existente) checked @endif>
+                        <label class="btn btn-outline-danger text-uppercase" for="btnNo-{{ $element->id }}">No</label>
+                      </div>
+                    </td>
+                    <td class="p-0">
+                      <div class="form-floating">
+                        <textarea rows="3" name="elemento{{ $i }}o" class="form-control rounded-0 resize-none fs-6 border-0"
+                          placeholder="Observación"
+                          id="inputSighting-{{ $element->id }}">{{ $element->observacion }}</textarea>
+                        <label for="inputSighting-{{ $element->id }}">Observación</label>
+                      </div>
+                    </td>
+                  </tr>
+                @endif
+              @endforeach
             @endforeach
-          @endforeach
-        </tbody>
-      </table>
-      <div class="mb-3">
-        <label for="building-format" class="form-label">Instalaciones</label>
+          </tbody>
+        </table>
+      </div>
+      <div class="my-3">
+        <label for="building-format" class="form-label fw-bold">Formato de Instalaciones</label>
         <input class="form-control" name="formatoInstalaciones" type="file" id="building-format" required>
       </div>
       <div class="d-flex justify-content-end">
-        <input class="btn btn-success" type="submit" value="Guardar">
+        <input class="btn boton-green text-light" type="submit" value="Guardar">
       </div>
     </form>
   </div>
@@ -89,11 +93,19 @@
         checkBox.value = this.checked
       }
 
-      if(checkBox.value == 'true'){
+      if (checkBox.value == 'true') {
         inputSighting.required = false
       } else {
         inputSighting.required = true
       }
+
+      $(document).ready(function() {
+        $('#elementsTable').DataTable({
+          "scrollY": "50vh",
+          "scrollCollapse": true,
+        });
+        $('.dataTables_length').addClass('bs-select');
+      });
     }
   </script>
 @endsection
