3c14b9922a31431d5883f22315bf88ad64caff4e
Victor Lascares <vmlascares@gmail.com>
2022-09-15 17:45:26 -0500

    Agregando modalidades y tipos de periodos a la tabla de carreras

--- database/migrations/2022_06_21_143422_create_careers_table.php
+++ database/migrations/2022_06_21_143422_create_careers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('careers', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->nullable(false);
-            $table->enum('modalidad', ['Presencial', 'Distancia', 'Hibrida'])->nullable(false);
+            $table->enum('modalidad', ['Escolarizado','Semiescolarizado','No escolarizado','Dual'])->nullable(false);
             $table->enum('tipoPeriodo',['Semestral','Cuatrimestral'])->nullable(false);
             $table->integer('numPeriodo')->nullable(false);
             $table->foreignId('institution_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
