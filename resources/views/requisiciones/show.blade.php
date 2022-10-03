3ee19d0163fabf14ae5f4a24b62b2e6a61a7826c
VictorLascares <vmlascares@gmail.com>
2022-10-03 14:23:16 -0500

    Modificando vista de requisicion

--- resources/views/requisiciones/show.blade.php
+++ resources/views/requisiciones/show.blade.php
@@ -7,32 +7,32 @@
     <img src="{{ $institution->logotipo }}" alt="Logotipo Institución">
   </div>
   <div class="md:grid md:grid-cols-2 md:gap-4">
-    <div class="border">
+    <div class="border rounded-md shadow-lg">
       <div class="border-b">
         <h2 class="text-gray-600 md:col-span-2 text-center p-3 text-xl font-bold uppercase">Información de la Institución
         </h2>
       </div>
       <p class="p-2 text-lg font-bold">Nombre: <span class="font-normal">{{ $institution->nombre }}</span></p>
       <p class="p-2 text-lg font-bold">Titular: <span class="font-normal">{{ $institution->titular }}</span></p>
-      <p class="p-2 text-lg font-bold">Representante legal o Asociacion Civil: <span
+      <p class="p-2 text-lg font-bold">Representante legal o Asociación Civil: <span
           class="font-normal">{{ $institution->repLegal }}</span></p>
       <p class="p-2 text-lg font-bold">Correo Institucional: <span class="font-normal">{{ $institution->email }}</span>
       </p>
       <p class="p-2 text-lg font-bold">Dirección: <span class="font-normal">{{ $institution->direccion }}</span></p>
     </div>
-    <div class="border mt-4 md:mt-0">
+    <div class="border mt-4 md:mt-0 rounded-md shadow-lg">
       <div class="border-b">
         <h2 class="text-gray-600 text-center p-3 text-xl font-bold uppercase">Información de la Carrera</h2>
       </div>
       <p class="p-2 text-lg font-bold">Nombre: <span class="font-normal">{{ $career->nombre }}</span></p>
       <p class="p-2 text-lg font-bold">Modalidad: <span class="font-normal">{{ $career->modalidad }}</span></p>
-      <p class="p-2 text-lg font-bold">Duracion: <span class="font-normal">{{ $career->numPeriodo }} @if ($career->tipoPeriodo == 'Semestral')
+      <p class="p-2 text-lg font-bold">Duración: <span class="font-normal">{{ $career->numPeriodo }} @if ($career->tipoPeriodo == 'Semestral')
             Semestres
           @elseif($career->tipoPeriodo == 'Cuatrimestral')
             Cuatrimestres
           @endif
         </span></p>
-      <p class="p-2 text-lg font-bold">Area: <span class="font-normal">{{ $area->nombre }}</span></p>
+      <p class="p-2 text-lg font-bold">Área: <span class="font-normal">{{ $area->nombre }}</span></p>
     </div>
   </div>
 
@@ -72,7 +72,6 @@
                 <option value="latencia">Latencia</option>
                 <option value="revocado">Revocado</option>
               @break
-
               @case('latencia')
               @case('revocado')
                 <option value="activo">Activo</option>
@@ -91,51 +90,51 @@
     <h2 class="text-center mb-5 uppercase text-2xl">Evaluación de la Solicitud</h2>
     <div class="grid sm:grid-cols-2 lg:grid-cols-6 gap-4">
       <a href="{{ route('evaluate.formats', $data->id) }}" type="button" id="evaFormatos"
-        class="p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 1) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if (Auth()->user()->tipoUsuario == 'direccion') disabled @endif">
+        class="rounded-md p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 1) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if (Auth()->user()->tipoUsuario == 'direccion') disabled @endif">
         <p class="uppercase">Formatos</p>
-        <p>Evaluación <span class="font-bold">1</span></p>
+        <p>Revisión <span class="font-bold">1</span></p>
       </a>
       <a href="{{ route('evaluate.formats', $data->id) }}"
-        class="p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 2) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 2 || Auth()->user()->tipoUsuario == 'direccion') disabled @endif">
+        class="rounded-md p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 2) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 2 || Auth()->user()->tipoUsuario == 'direccion') disabled @endif">
         <p class="uppercase">Formatos</p>
-        <p>Evaluación <span class="font-bold">2</span></p>
+        <p>Revisión <span class="font-bold">2</span></p>
       </a>
       <a href="{{ route('evaluate.formats', $data->id) }}"
-        class="p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 3) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if (Auth()->user()->tipoUsuario == 'planeacion' || $data->estado == 'rechazado') disabled @endif">
+        class="rounded-md p-3 text-white formatos bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 3) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if (Auth()->user()->tipoUsuario == 'planeacion' || $data->estado == 'rechazado') disabled @endif">
         <p class="uppercase">Formatos</p>
-        <p>Evaluación <span class="font-bold">3</span></p>
+        <p>Revisión <span class="font-bold">3</span></p>
       </a>
       <a href="{{ url('/evaluate/elements', $data->id) }}"
-        class="p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 4) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 4 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
+        class="rounded-md p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 4) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 4 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
         <p class="uppercase">Instalaciones</p>
-        <p>Evaluación <span class="font-bold">4</span></p>
+        <p>Evaluación <span class="font-bold">2</span></p>
       </a>
       <a href="{{ url('/evaluate/plans', $data->id) }}"
-        class="p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 5) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 5 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
+        class="rounded-md p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 5) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 5 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
         <p class="uppercase">Planes</p>
-        <p>Evaluación <span class="font-bold">5</span></p>
+        <p>Evaluación <span class="font-bold">3</span></p>
       </a>
       <a download="OTAReq-{{ $data->id }}" href="{{ url('/download', $data->id) }}"
-        class="p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 6) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 6 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
+        class="rounded-md p-3 text-white bg-[#13322B] hover:bg-[#0C231E] @if ($data->noEvaluacion == 6) bg-blue-500 hover:bg-blue-800 @endif flex flex-col justify-center items-center @if ($data->noEvaluacion < 6 || Auth()->user()->tipoUsuario == 'planeacion') disabled @endif">
+        <p>Descargar</p>
         <p class="uppercase">ota</p>
-        <p>Evaluación <span class="font-bold">6</span></p>
       </a>
     </div>
 
     @if (Auth::user()->tipoUsuario == 'administrador')
       <div class="flex justify-end mt-4">
         <a href="{{ url('/evaluacion-anterior', $data->id) }}"
-          class="py-2 px-4 bg text-white bg-[#13322B] hover:bg-[#0C231E]">Modificar
+          class="rounded-md py-2 px-4 bg text-white bg-[#13322B] hover:bg-[#0C231E]">Modificar
           Evaluación</a>
       </div>
     @endif
   </div>
 
   @if (!empty($errors))
-    <div class="pb-4">
+    <div class="pb-4 w-4/6 mx-auto text-center">
       @foreach ($errors as $error)
         @if ($error->justificacion)
-          <p class="bg-red-400 p-4 text-red-900">Revision {{ $data->noEvaluacion - 1 }}.- Evaluación de Formatos -
+          <p class="bg-red-400 p-4 text-red-900 mb-4">Revision {{ $data->noEvaluacion - 1 }}.- Evaluación de Formatos -
             {{ $error->justificacion }} ({{ $formatNames[$error->formato - 1] }})</p>
         @elseif ($error->observacion)
           <p class="bg-red-400 p-4 text-red-900">Evaluación de las Instalaciones.- {{ $error->observacion }} (Elemento
