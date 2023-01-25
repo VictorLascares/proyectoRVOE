a603697399e77228a9b7e2e8bb94239b73579536
Victor Lascares <vmlascares@gmail.com>
2023-01-24 22:46:25 -0600

    Actualizando los criterios de evaluacion

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -29,9 +29,26 @@ class PlanController extends Controller
             $institution = Institution::find($career->institution_id);
             $plans = Plan::searchrequisitionid($requisition->id)->get();
             $planNames = array(
-                "Planes y programas de estudios",
-                "Reglamentos",
-                "Plan de mejora continua"
+                ["aspecto" => "Grado académico", "totalPts" => 1],
+                ["aspecto" => "Modalidad educativa", "totalPts" => 1],
+                ["aspecto" => "Área de formación", "totalPts" => 1],
+                ["aspecto" => "Duración mínima en semanas", "totalPts" => 1],
+                ["aspecto" => "Carga horaria semanal", "totalPts" => 5],
+                ["aspecto" => "Tipo de diseño", "totalPts" => 1],
+                ["aspecto" => "Número de ciclos escolares que integran el plan de estudios", "totalPts" => 1],
+                ["aspecto" => "Indica los fines del aprendizaje", "totalPts" => 12],
+                ["aspecto" => "Perfil de ingreso", "totalPts" => 10],
+                ["aspecto" => "Antecedentes académico de ingreso", "totalPts" => 1],
+                ["aspecto" => "Perfil de egreso", "totalPts" => 12],
+                ["aspecto" => "Requisitos de ingreso", "totalPts" => 2],
+                ["aspecto" => "Requisitos de permanencia", "totalPts" => 10],
+                ["aspecto" => "Estrategias y lineamientos para asegurar el ingreso, permanencia, egreso y titulación", "totalPts" => 10],
+                ["aspecto" => "Proyección de la matrícula escolar", "totalPts" => 5],
+                ["aspecto" => "Modelo educativo", "totalPts" => 30],
+                ["aspecto" => "Modalidad de evaluación del plan de estudios", "totalPts" => 20],
+                ["aspecto" => "Periodicidad de evaluación del plan de estudios", "totalPts" => 5],
+                ["aspecto" => "Justificación de la modalidad elegida", "totalPts" => 20],
+                ["aspecto" => "Mapa curricular", "totalPts" => 40]
             );
 
             return view('requisiciones.plansEva', compact('requisition', 'career', 'institution', 'plans', 'planNames'));
@@ -52,7 +69,7 @@ class PlanController extends Controller
                     $plan->comentario = $request->input($planNumberc);
                     $plan->save();
                 }
-                if($requisition->noEvaluacion == 5){
+                if ($requisition->noEvaluacion == 5) {
                     $requisition->noEvaluacion = $requisition->noEvaluacion + 1;
                 }
                 $requisition->save();
