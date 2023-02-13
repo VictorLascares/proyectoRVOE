403789e5458258c81249ab43957b822245c6ad33
Victor Lascares <vmlascares@gmail.com>
2023-02-13 15:17:24 -0600

    Acomando las revisiones y desactivando el boton de guardar de forma condicional

--- resources/views/requisiciones/formEva.blade.php
+++ resources/views/requisiciones/formEva.blade.php
@@ -12,47 +12,84 @@
     <div class="my-20">
         <form method="POST" action="{{ url('/update/formats') }}">
             @csrf
-            <div @if ($noEvaluation == 1) class="flex flex-col justify-center items-center" @endif>
-                <input type="hidden" name="requisition_id" value="{{ $requisition->id }}">
-                <input type="hidden" name="noEvaluation" value="{{ $noEvaluation }}">
-                <div class="@if ($noEvaluation != 1) grid grid-cols-2 @endif gap-4">
+
+            <input type="hidden" name="requisition_id" value="{{ $requisition->id }}">
+            <input type="hidden" name="noEvaluation" value="{{ $noEvaluation }}">
+            {{-- Revision 1 --}}
+            @if ($noEvaluation == 1)
+                <table class="min-w-full">
+                    <thead class="border-b bg-green-900">
+                        <tr>
+                            <th class="text-sm font-bold text-white px-6 py-4 text-left">#</th>
+                            <th class="text-sm font-bold text-white px-6 py-4 text-center">Existe</th>
+                            <th class="text-sm font-bold text-white px-6 py-4 text-center">Nombre del formato</th>
+                            <th class="text-sm font-bold text-white px-6 py-4 text-center">Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach (range(1, 5) as $i)
+                            @foreach ($formats as $format)
+                                @if ($format->format == $i)
+                                    <tr class="border-b">
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $i }}</td>
+                                        <td class="text-center ">
+                                            <input name="anexo{{ $i }}" value="{{ $format->valid }}"
+                                                class="reviewCheckbox form-check-input" type="checkbox"
+                                                id="check-review-{{ $format->id }}">
+                                        </td>
+                                        <td class="text-sm text-gray-900 px-6 py-4 whitespace-nowrap text-center">
+                                            <label class="form-check-label" for="check-review2-{{ $format->id }}">
+                                                {{ $formatNames[$i - 1] }}
+                                            </label>
+                                        </td>
+                                        <td class="text-sm px-6 py-4 whitespace-nowrap text-center">
+                                            @if ($format->valid)
+                                                <p class="text-green-500">Registrado</p>
+                                            @elseif ($format->justification)
+                                                <p class="text-red-500">{{ $format->justification }}</p>
+                                            @else
+                                                <p class="text-red-500">No registrado</p>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                        @endforeach
+                    </tbody>
+                </table>
+            @elseif($noEvaluation == 2 || $noEvaluation == 3)
+                {{-- Revision 2 y 3 --}}
+                <div class="grid grid-cols-2 gap-4 w-full">
                     @foreach (range(1, 5) as $i)
                         @foreach ($formats as $format)
                             @if ($format->format == $i)
-                                <div class="@if ($i == 5) col-span-2 @endif">
-                                    <div class="mb-4  flex justify-start items-center gap-4">
-                                        @if ($noEvaluation == 1)
-                                            <input name="anexo{{ $i }}" value="{{ $format->valid }}"
-                                                class="reviewCheckbox form-check-input" type="checkbox"
-                                                id="check-review-{{ $format->id }}">
-                                        @elseif($noEvaluation != 1 && $format->valid)
-                                            <input name="anexo{{ $i }}" value="{{ $format->valid }}"
-                                                class="reviewCheckbox form-check-input" type="checkbox"
-                                                id="check-review-{{ $format->id }}">
-                                        @endif
+                                <div class="flex flex-col gap-2">
+                                    <div class="flex gap-2 items-center">
+                                        <input name="anexo{{ $i }}" value="{{ $format->valid }}"
+                                            class="reviewCheckbox form-check-input @if ($noEvaluation ==2 && $format->justification == "No se encuentra el formato") disabled @endif" type="checkbox"
+                                            id="check-review-{{ $format->id }}">
                                         <label class="form-check-label" for="check-review2-{{ $format->id }}">
                                             {{ $formatNames[$i - 1] }}
-                                            @if ($format->valid == false && $format->justification)
-                                                <span class="text-red-600">({{ $format->justification }})</span>
-                                            @elseif ($format->valid == true)
-                                                <span class="text-green-400">&#10003; {{ $format->justification }}</span>
-                                            @endif
                                         </label>
+
+                                        @if ($format->valid)
+                                            <p class="text-green-500">&#10003; {{ $format->justification}}</p>
+                                        @else
+                                            <p class="text-red-500">({{ $format->justification }})</p>
+                                        @endif
                                     </div>
-                                    @if ($noEvaluation != 1 && $format->valid)
-                                        <div>
-                                            <textarea name="anexo{{ $i }}j" class="w-full resize-none" id="just-review-{{ $format->id }}"
-                                                placeholder="Justificación"></textarea>
-                                        </div>
-                                    @endif
+                                    <textarea id="just-review-{{$format->id}}" name="anexo{{ $i }}j" rows="3"
+                                        class="w-full resize-none focus:ring-0 border-2 border-gray-200 rounded-xl @if ($noEvaluation ==2 && $format->justification == "No se encuentra el formato") disabled @endif" placeholder="Observacion"></textarea>
                                 </div>
                             @endif
                         @endforeach
                     @endforeach
                 </div>
-                <div class="flex justify-center items-center mt-4">
-                    <button class="text-white bg-green-900 hover:bg-green-700 px-10 py-3" type="submit">Guardar</button>
-                </div>
+            @endif
+
+            <div class="@if ($requisition->evaNum > 2 && Auth()->user()->typeOfUser == 'planeacion') disabled @endif flex justify-end items-center mt-4">
+                <button class="text-white bg-green-900 hover:bg-green-700 px-10 py-3" type="submit">Guardar</button>
             </div>
         </form>
     </div>
