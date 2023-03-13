be956f627320fa093ada8c2f8abdba90d7061cc5
Victor Lascares <vmlascares@gmail.com>
2023-03-13 17:08:55 -0600

    Actualizando logout

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
     public function store()
     {
-        auth()->logout();
+        Auth::logout();
+        // auth()->logout();
         return redirect()->route('login');
     }
 }
